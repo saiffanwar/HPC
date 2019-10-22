08925bbd42c25b9159a29ec55347179d4b753b2c
Saif Anwar <anwarsaif99@hotmail.com>
2019-10-22 15:59:55 +0100

    allignment

--- stencil.c
+++ stencil.c
@@ -58,7 +58,7 @@ int main(int argc, char* argv[])
 }
 
 void stencil(const int nx, const int ny, const int width, const int height,
-             float* image, float* tmp_image)
+             float* restrict image, float* restrict tmp_image)
 //with floating points
 {
 /*__assume_aligned(image, 16);
@@ -87,9 +87,11 @@ __assume_aligned(tmp_image,16);
 ///////////TRYING ALIGNMENTS//////////////
 __assume_aligned(image, 16);
 __assume_aligned(tmp_image,16);
+__assume(height%16==0)
+__assume(1%16==0)
   for (int j = 1; j < ny + 1; ++j) {
     for (int i = 1; i < ny + 1; ++i) {
-      tmp_image[j + i * height] =  image[j     + i       * height] * 3.0f / 5.0f + image[j     + (i - 1) * height] * 0.5f / 5.0f + image[j     + (i + 1) * height] * 0.5f / 5.0f + image[j - 1 + i       * height] * 0.5f / 5.0f + image[j + 1 + i       * height] * 0.5f / 5.0f;
+      tmp_image[j + i * height] +=  image[j     + i       * height] * 3.0f / 5.0f + image[j     + (i - 1) * height] * 0.5f / 5.0f + image[j     + (i + 1) * height] * 0.5f / 5.0f + image[j - 1 + i       * height] * 0.5f / 5.0f + image[j + 1 + i       * height] * 0.5f / 5.0f;
     }
 }
 }

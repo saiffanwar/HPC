00f4992987239f28f7d838a04690c50fc4feb3ee
Saif Anwar <anwarsaif99@hotmail.com>
2019-10-22 16:25:25 +0100

    attempt 2

--- stencil.c
+++ stencil.c
@@ -64,8 +64,10 @@ void stencil(const int nx, const int ny, const int width, const int height,
              float* restrict image, float* restrict tmp_image)
 //with floating points
 {
-/*__assume_aligned(image, 16);
-__assume_aligned(tmp_image,16);
+  __assume_aligned(image, 16);
+  __assume_aligned(tmp_image,16);
+  __assume(height%16==0);
+  __assume(1%16==0);
   for (int j = 1; j < ny + 1; ++j) {
     for (int i = 1; i < ny + 1; ++i) {
       tmp_image[j + i * height] =  image[j     + i       * height] * 3.0f / 5.0f;
@@ -74,7 +76,7 @@ __assume_aligned(tmp_image,16);
       tmp_image[j + i * height] += image[j - 1 + i       * height] * 0.5f / 5.0f;
       tmp_image[j + i * height] += image[j + 1 + i       * height] * 0.5f / 5.0f;
     }
-} */
+}
 //without floating points
 //{
 //  for (int j = 1; j < ny + 1; ++j) {
@@ -87,16 +89,6 @@ __assume_aligned(tmp_image,16);
 //    }
 //  }
 
-///////////TRYING ALIGNMENTS//////////////
-__assume_aligned(image, 16);
-__assume_aligned(tmp_image,16);
-__assume(height%16==0);
-__assume(1%16==0);
-  for (int j = 1; j < ny + 1; ++j) {
-    for (int i = 1; i < ny + 1; ++i) {
-      tmp_image[j + i * height] +=  image[j     + i       * height] * 3.0f / 5.0f + image[j     + (i - 1) * height] * 0.5f / 5.0f + image[j     + (i + 1) * height] * 0.5f / 5.0f + image[j - 1 + i       * height] * 0.5f / 5.0f + image[j + 1 + i       * height] * 0.5f / 5.0f;
-    }
-}
 }
 
 

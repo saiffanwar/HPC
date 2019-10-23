033202e67e9ef6b272a13e5d11402cdeb5dfe201
Saif Anwar <anwarsaif99@hotmail.com>
2019-10-23 15:00:41 +0100

    update

--- stencil.c
+++ stencil.c
@@ -63,11 +63,11 @@ void stencil(const int nx, const int ny, const int width, const int height,
 {
 #pragma vector aligned
 #pragma ivdep
-#pragma simd
   __assume_aligned(image, 32);
   __assume_aligned(tmp_image,32);
   __assume(height%16==0);
   //__assume(1%32==0);
+  #pragma simd
   for (int j = 1; j < ny + 1; ++j) {
     for (int i = 1; i < nx + 1; ++i) {
       tmp_image[j + i * height] =  image[j     + i       * height] * 3.0f / 5.0f;

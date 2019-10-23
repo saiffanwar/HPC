e08d52e1f9bfe9f1a42f9498de564512cc0d0f6c
Saif Anwar <anwarsaif99@hotmail.com>
2019-10-23 16:23:46 +0100

    changed back to int

--- stencil.c
+++ stencil.c
@@ -5,7 +5,7 @@
 // Define output file name
 #define OUTPUT_FILE "stencil.pgm"
 
-void stencil(const short nx, const short ny, const short width, const short height,
+void stencil(const int nx, const int ny, const int width, const int height,
              float* image, float* tmp_image);
 void init_image(const int nx, const int ny, const int width, const int height,
                 float* image, float* tmp_image);
@@ -57,21 +57,22 @@ int main(int argc, char* argv[])
   _mm_free(tmp_image);
 }
 
-void stencil(const short nx, const short ny, const short width, const short height,
+void stencil(const int nx, const int ny, const int width, const int height,
              float* image, float* tmp_image)
 //with floating points
 {
-#pragma vector aligned
-#pragma ivdep
-#pragma simd
  // __assume_aligned(image, 32);
  // __assume_aligned(tmp_image,32);
  // __assume(height%16==0);
   //__assume(1%32==0);
-  for (short j = 1; j < ny + 1; ++j) {
-    for (short i = 1; i < nx + 1; ++i) {
+  for (int j = 1; j < ny + 1; ++j) {
+    #pragma vector aligned
+    #pragma ivdep
+    #pragma simd
+    for (int i = 1; i < nx + 1; ++i) {
       tmp_image[j + i * height] =  image[j     + i       * height] * 3.0f / 5.0f;
       tmp_image[j + i * height] += image[j     + (i - 1) * height] * 0.5f / 5.0f;
+      for (int)
       tmp_image[j + i * height] += image[j     + (i + 1) * height] * 0.5f / 5.0f;
       tmp_image[j + i * height] += image[j - 1 + i       * height] * 0.5f / 5.0f;
       tmp_image[j + i * height] += image[j + 1 + i       * height] * 0.5f / 5.0f;

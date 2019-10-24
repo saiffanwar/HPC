5c4bb5b4ef81a675dcc052959d10c329092650c5
Saif Anwar <anwarsaif99@hotmail.com>
2019-10-24 20:06:02 +0100

    reducing loads

--- stencil.c
+++ stencil.c
@@ -65,7 +65,7 @@ int main(int argc, char* argv[])
   //  #pragma simd
   //__assume_aligned(image, 16);
   //__assume_aligned(tmp_image,16);
-    for (int i = 1; i < nx + 1; ++i) {
+/*    for (int i = 1; i < nx + 1; ++i) {
       for (int j = 1; j < ny + 1; ++j) {
         tmp_image[j + i * height] =  image[j     + i       * height] * 0.6f;
         tmp_image[j + i * height] += image[j     + (i - 1) * height] * 0.1f;
@@ -74,6 +74,12 @@ int main(int argc, char* argv[])
         tmp_image[j + i * height] += image[j + 1 + i       * height] * 0.1f;
       }
   }
+*/
+  for (int i = 1; i < nx + 1; ++i) {
+    for (int j = 1; j < ny + 1; ++j) {
+      tmp_image[j + i * height] =  image[j     + i       * height] * 0.6f + image[j     + (i - 1) * height] * 0.1f + image[j     + (i + 1) * height] * 0.1f + image[j - 1 + i       * height] * 0.1f + image[j + 1 + i       * height] * 0.1f;
+    }
+}
   //without floating points
   //{
   //  for (int j = 1; j < ny + 1; ++j) {

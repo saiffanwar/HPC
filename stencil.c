7d46a8ccd61e4b3aa026259bfe2c4dcb4782c064
Saif Anwar <anwarsaif99@hotmail.com>
2019-12-10 16:33:13 +0000

    fixing indenting

--- stencil.c
+++ stencil.c
@@ -123,7 +123,7 @@ int main(int argc, char* argv[])
       stencil(nx, ny, width, height, tmp_image, image);
       halo_exchange(rank);
     }
-    double toc = wtime();    
+    double toc = wtime();
     // Output
     printf("------------------------------------\n");
     printf(" runtime: %lf s\n", toc - tic);
@@ -133,106 +133,106 @@ int main(int argc, char* argv[])
     free(image);
     free(tmp_image);
   }
-  }
-
-  void stencil(const int nx, const int ny, const int width, const int height,
-               float* restrict image, float* restrict tmp_image)
-  //with floating points
-  {
-
-  for (int i = 1; i < nx + 1; ++i) {
-    for (int j = 1; j < ny + 1; ++j) {
-      tmp_image[j + i * height] =  image[j+i* height] * 0.6f + image[j+ (i - 1) * height] * 0.1f + image[j+ (i + 1) * height] * 0.1f + image[j - 1 + i * height] * 0.1f + image[j + 1 + i * height] * 0.1f;
-    }
 }
+
+void stencil(const int nx, const int ny, const int width, const int height,
+             float* restrict image, float* restrict tmp_image)
+//with floating points
+{
+
+for (int i = 1; i < nx + 1; ++i) {
+  for (int j = 1; j < ny + 1; ++j) {
+    tmp_image[j + i * height] =  image[j+i* height] * 0.6f + image[j+ (i - 1) * height] * 0.1f + image[j+ (i + 1) * height] * 0.1f + image[j - 1 + i * height] * 0.1f + image[j + 1 + i * height] * 0.1f;
+  }
+}
+}
+
+void halo_exchange(int rank) {
+  for(ii=0; ii < local_nrows; ii++) {
+    sendbuf[ii] = subgrid[ii * (local_ncols + 2) + 1];
+  MPI_Sendrecv(sendbuf, local_nrows, MPI_FLOAT, left, tag, recvbuf, local_nrows, MPI_DOUBLE, right, tag, MPI_COMM_WORLD, &status);
+  }
+  for(ii=0; ii < local_nrows; ii++){
+    subgrid[ii * (local_ncols + 2) + local_ncols + 1] = recvbuf[ii];
+  }
+  /* send to the right, receive from left */
+  for(ii=0; ii < local_nrows; ii++){
+    sendbuf[ii] = subgrid[ii * (local_ncols + 2) + local_ncols];
+  MPI_Sendrecv(sendbuf, local_nrows, MPI_DOUBLE, right, tag, recvbuf, local_nrows, MPI_DOUBLE, left, tag, MPI_COMM_WORLD, &status);
+  }
+  for(ii=0; ii < local_nrows; ii++){
+    subgrid[ii * (local_ncols + 2)] = recvbuf[ii];
+  }
+}
+
+// Create the input image
+void init_image(const int nx, const int ny, const int width, const int height,
+                float* image, float* tmp_image)
+{
+  // Zero everything
+  for (int j = 0; j < ny + 2; ++j) {
+    for (int i = 0; i < nx + 2; ++i) {
+      image[j + i * height] = 0.0f;
+      tmp_image[j + i * height] = 0.0f;
+    }
   }
 
-  void halo_exchange(int rank) {
-    for(ii=0; ii < local_nrows; ii++) {
-      sendbuf[ii] = subgrid[ii * (local_ncols + 2) + 1];
-    MPI_Sendrecv(sendbuf, local_nrows, MPI_FLOAT, left, tag, recvbuf, local_nrows, MPI_DOUBLE, right, tag, MPI_COMM_WORLD, &status);
-    }
-    for(ii=0; ii < local_nrows; ii++){
-      subgrid[ii * (local_ncols + 2) + local_ncols + 1] = recvbuf[ii];
-    }
-    /* send to the right, receive from left */
-    for(ii=0; ii < local_nrows; ii++){
-      sendbuf[ii] = subgrid[ii * (local_ncols + 2) + local_ncols];
-    MPI_Sendrecv(sendbuf, local_nrows, MPI_DOUBLE, right, tag, recvbuf, local_nrows, MPI_DOUBLE, left, tag, MPI_COMM_WORLD, &status);
-    }
-    for(ii=0; ii < local_nrows; ii++){
-      subgrid[ii * (local_ncols + 2)] = recvbuf[ii];
-    }
-  }
-
-  // Create the input image
-  void init_image(const int nx, const int ny, const int width, const int height,
-                  float* image, float* tmp_image)
-  {
-    // Zero everything
-    for (int j = 0; j < ny + 2; ++j) {
-      for (int i = 0; i < nx + 2; ++i) {
-        image[j + i * height] = 0.0f;
-        tmp_image[j + i * height] = 0.0f;
-      }
-    }
-
-  const int tile_size = 64;
-  // checkerboard pattern
-  for (int jb = 0; jb < ny; jb += tile_size) {
-    for (int ib = 0; ib < nx; ib += tile_size) {
-      if ((ib + jb) % (tile_size * 2)) {
-        const int jlim = (jb + tile_size > ny) ? ny : jb + tile_size;
-        const int ilim = (ib + tile_size > nx) ? nx : ib + tile_size;
-        for (int j = jb + 1; j < jlim + 1; ++j) {
-          for (int i = ib + 1; i < ilim + 1; ++i) {
-            image[j + i * height] = 100.0f;
-          }
+const int tile_size = 64;
+// checkerboard pattern
+for (int jb = 0; jb < ny; jb += tile_size) {
+  for (int ib = 0; ib < nx; ib += tile_size) {
+    if ((ib + jb) % (tile_size * 2)) {
+      const int jlim = (jb + tile_size > ny) ? ny : jb + tile_size;
+      const int ilim = (ib + tile_size > nx) ? nx : ib + tile_size;
+      for (int j = jb + 1; j < jlim + 1; ++j) {
+        for (int i = ib + 1; i < ilim + 1; ++i) {
+          image[j + i * height] = 100.0f;
         }
       }
     }
   }
 }
+}
 
 // Routine to output the image in Netpbm grayscale binary image format
 void output_image(const char* file_name, const int nx, const int ny,
-                  const int width, const int height, float* image)
+                const int width, const int height, float* image)
 {
-  // Open output file
-  FILE* fp = fopen(file_name, "w");
-  if (!fp) {
-    fprintf(stderr, "Error: Could not open %s\n", OUTPUT_FILE);
-    exit(EXIT_FAILURE);
+// Open output file
+FILE* fp = fopen(file_name, "w");
+if (!fp) {
+  fprintf(stderr, "Error: Could not open %s\n", OUTPUT_FILE);
+  exit(EXIT_FAILURE);
+}
+
+// Ouptut image header
+fprintf(fp, "P5 %d %d 255\n", nx, ny);
+
+// Calculate maximum value of image
+// This is used to rescale the values
+// to a range of 0-255 for output
+double maximum = 0.0;
+for (int j = 1; j < ny + 1; ++j) {
+  for (int i = 1; i < nx + 1; ++i) {
+    if (image[j + i * height] > maximum) maximum = image[j + i * height];
   }
+}
 
-  // Ouptut image header
-  fprintf(fp, "P5 %d %d 255\n", nx, ny);
-
-  // Calculate maximum value of image
-  // This is used to rescale the values
-  // to a range of 0-255 for output
-  double maximum = 0.0;
-  for (int j = 1; j < ny + 1; ++j) {
-    for (int i = 1; i < nx + 1; ++i) {
-      if (image[j + i * height] > maximum) maximum = image[j + i * height];
-    }
+// Output image, converting to numbers 0-255
+for (int j = 1; j < ny + 1; ++j) {
+  for (int i = 1; i < nx + 1; ++i) {
+    fputc((char)(255.0f * image[j + i * height] / maximum), fp);
   }
+}
 
-  // Output image, converting to numbers 0-255
-  for (int j = 1; j < ny + 1; ++j) {
-    for (int i = 1; i < nx + 1; ++i) {
-      fputc((char)(255.0f * image[j + i * height] / maximum), fp);
-    }
-  }
-
-  // Close the file
-  fclose(fp);
+// Close the file
+fclose(fp);
 }
 
 // Get the current time in seconds since the Epoch
 double wtime(void)
 {
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  return tv.tv_sec + tv.tv_usec * 1e-6;
+struct timeval tv;
+gettimeofday(&tv, NULL);
+return tv.tv_sec + tv.tv_usec * 1e-6;
 }

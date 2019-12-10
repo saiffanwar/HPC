6d1718b71f2c57cb610976e4d0ea5c6b0ba2433b
Saif Anwar <anwarsaif99@hotmail.com>
2019-12-10 17:36:33 +0000

    finalize mpi

--- stencil.c
+++ stencil.c
@@ -6,15 +6,13 @@
 #define OUTPUT_FILE "stencil.pgm"
 #define MASTER 0
 
-int calc_ncols_from_rank(int rank, int size);
-
-
 void stencil(const int nx, const int ny, const int width, const int height,
              float* image, float* tmp_image);
 void init_image(const int nx, const int ny, const int width, const int height,
                 float* image, float* tmp_image);
 void output_image(const char* file_name, const int nx, const int ny,
                   const int width, const int height, float* image);
+void halo_exchange(int rank);
 double wtime(void);
 
 int ii,jj;             /* row and column indices for the grid */
@@ -83,7 +81,6 @@ int main(int argc, char* argv[])
   if (argc != 4) {
     fprintf(stderr, "Usage: %s nx ny niters\n", argv[0]);
     exit(EXIT_FAILURE);
-    MPI_Finalize();
   }
 
   // Initiliase problem dimensions from command line arguments
@@ -133,6 +130,7 @@ int main(int argc, char* argv[])
     free(image);
     free(tmp_image);
   }
+  MPI_Finalize();
 }
 
 void stencil(const int nx, const int ny, const int width, const int height,
@@ -140,11 +138,11 @@ void stencil(const int nx, const int ny, const int width, const int height,
 //with floating points
 {
 
-for (int i = 1; i < nx + 1; ++i) {
-  for (int j = 1; j < ny + 1; ++j) {
-    tmp_image[j + i * height] =  image[j+i* height] * 0.6f + image[j+ (i - 1) * height] * 0.1f + image[j+ (i + 1) * height] * 0.1f + image[j - 1 + i * height] * 0.1f + image[j + 1 + i * height] * 0.1f;
+  for (int i = 1; i < nx + 1; ++i) {
+    for (int j = 1; j < ny + 1; ++j) {
+      tmp_image[j + i * height] =  image[j+i* height] * 0.6f + image[j+ (i - 1) * height] * 0.1f + image[j+ (i + 1) * height] * 0.1f + image[j - 1 + i * height] * 0.1f + image[j + 1 + i * height] * 0.1f;
+    }
   }
-}
 }
 
 void halo_exchange(int rank) {
@@ -177,56 +175,56 @@ void init_image(const int nx, const int ny, const int width, const int height,
     }
   }
 
-const int tile_size = 64;
-// checkerboard pattern
-for (int jb = 0; jb < ny; jb += tile_size) {
-  for (int ib = 0; ib < nx; ib += tile_size) {
-    if ((ib + jb) % (tile_size * 2)) {
-      const int jlim = (jb + tile_size > ny) ? ny : jb + tile_size;
-      const int ilim = (ib + tile_size > nx) ? nx : ib + tile_size;
-      for (int j = jb + 1; j < jlim + 1; ++j) {
-        for (int i = ib + 1; i < ilim + 1; ++i) {
-          image[j + i * height] = 100.0f;
+  const int tile_size = 64;
+  // checkerboard pattern
+  for (int jb = 0; jb < ny; jb += tile_size) {
+    for (int ib = 0; ib < nx; ib += tile_size) {
+      if ((ib + jb) % (tile_size * 2)) {
+        const int jlim = (jb + tile_size > ny) ? ny : jb + tile_size;
+        const int ilim = (ib + tile_size > nx) ? nx : ib + tile_size;
+        for (int j = jb + 1; j < jlim + 1; ++j) {
+          for (int i = ib + 1; i < ilim + 1; ++i) {
+            image[j + i * height] = 100.0f;
+          }
         }
       }
     }
   }
-}
 }
 
 // Routine to output the image in Netpbm grayscale binary image format
 void output_image(const char* file_name, const int nx, const int ny,
                 const int width, const int height, float* image)
 {
-// Open output file
-FILE* fp = fopen(file_name, "w");
-if (!fp) {
-  fprintf(stderr, "Error: Could not open %s\n", OUTPUT_FILE);
-  exit(EXIT_FAILURE);
-}
-
-// Ouptut image header
-fprintf(fp, "P5 %d %d 255\n", nx, ny);
-
-// Calculate maximum value of image
-// This is used to rescale the values
-// to a range of 0-255 for output
-double maximum = 0.0;
-for (int j = 1; j < ny + 1; ++j) {
-  for (int i = 1; i < nx + 1; ++i) {
-    if (image[j + i * height] > maximum) maximum = image[j + i * height];
+  // Open output file
+  FILE* fp = fopen(file_name, "w");
+  if (!fp) {
+    fprintf(stderr, "Error: Could not open %s\n", OUTPUT_FILE);
+    exit(EXIT_FAILURE);
   }
-}
 
-// Output image, converting to numbers 0-255
-for (int j = 1; j < ny + 1; ++j) {
-  for (int i = 1; i < nx + 1; ++i) {
-    fputc((char)(255.0f * image[j + i * height] / maximum), fp);
+  // Ouptut image header
+  fprintf(fp, "P5 %d %d 255\n", nx, ny);
+
+  // Calculate maximum value of image
+  // This is used to rescale the values
+  // to a range of 0-255 for output
+  double maximum = 0.0;
+  for (int j = 1; j < ny + 1; ++j) {
+    for (int i = 1; i < nx + 1; ++i) {
+      if (image[j + i * height] > maximum) maximum = image[j + i * height];
+    }
   }
-}
 
-// Close the file
-fclose(fp);
+  // Output image, converting to numbers 0-255
+  for (int j = 1; j < ny + 1; ++j) {
+    for (int i = 1; i < nx + 1; ++i) {
+      fputc((char)(255.0f * image[j + i * height] / maximum), fp);
+    }
+  }
+
+  // Close the file
+  fclose(fp);
 }
 
 // Get the current time in seconds since the Epoch

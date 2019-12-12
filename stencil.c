7777617aa4f7479412f07933cfbf634d27967bc1
Saif Anwar <anwarsaif99@hotmail.com>
2019-12-12 02:08:41 +0000

    changed some values

--- stencil.c
+++ stencil.c
@@ -145,13 +145,13 @@ void stencil(const int nx, const int ny, const int width, const int height, cons
 //with floating points
 {
 MPI_Status status;
-MPI_Sendrecv(image+width, width, MPI_FLOAT, left, 4,
- image+((height-1)*width), width, MPI_FLOAT, right, 4,
+MPI_Sendrecv(image+width, width, MPI_FLOAT, left, 6,
+ image+((height-1)*width), width, MPI_FLOAT, right, 9,
  MPI_COMM_WORLD, &status);
 
 //send right, receive left
-MPI_Sendrecv(image+((height-2)*width), width, MPI_FLOAT, right, 20,
- image, width, MPI_FLOAT, left, 20,
+MPI_Sendrecv(image+((height-2)*width), width, MPI_FLOAT, right, 6,
+ image, width, MPI_FLOAT, left, 9,
  MPI_COMM_WORLD, &status);
 
   for (int i = 1; i < nx + 1; ++i) {
